34c12eef5bb7951f9fdcb1d6805dd3742487088d
Felipe Farias <fefarias97@gmail.com>
2021-08-22 20:08:56 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Indila - Dernière danse");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Oh ma douce souffrance");
+            Console.WriteLine("Pourquoi s'acharner? Tu recommences");
+            Console.WriteLine("Je ne suis qu'un être sans importance");
+            Console.WriteLine("Sans lui, je suis un peu paro");
+            Console.WriteLine("Je déambule seule dans le métro");
+            Console.WriteLine("");
+            Console.WriteLine("Une dernière danse");
+            Console.WriteLine("Pour oublier ma peine immense");
+            Console.WriteLine("Je veux m'enfuir que tout recommence");
+            Console.WriteLine("Oh ma douce souffrance");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Je remue le ciel, le jour, la nuit");
+            Console.WriteLine("Je danse avec le vent, la pluie");
+            Console.WriteLine("Un peu d'amour, un brin de miel");
+            Console.WriteLine("Et je danse, danse, danse, danse, danse, danse, danse");
+            Console.WriteLine();
+            Console.ResetColor();
+
+        }
+    }
+}
